eda3565e448a7f81af43fc48955d25ae9f143061
Jeremias Fernandez <51936346+Ella2024@users.noreply.github.com>
2023-05-24 11:30:38 -0300

    Eliminacion total
    si

--- Html/form.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('adoption-form');
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  function enviarFormulario() {
-    alert("Espera un momento, pronto te llamaremos.");
-  }
-});
